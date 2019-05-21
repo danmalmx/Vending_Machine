d3897a346e5a5b9e13bb3c8cb137b702cd22345e
danmalmx <danmalmx@gmail.com>
2019-05-21 10:32:35 +0200

    Adding derived class Snack and initialize method

---
+++ Vending_Machine/Vending_Machine/Snacks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vending_Machine
+{
+    class Snack : Products
+    {
+
+        public override void CreateProductGroup(Product ProductType, string Name, int Price, string Consuming)
+        {
+            ProductType = Product.Snack;
+            this.Name = Name;
+            this.Price = Price;
+            this.Consuming = Consuming;            
+        }
+    }
+}
