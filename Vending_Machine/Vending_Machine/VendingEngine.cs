94d3e42bf422b5c1fecd1019f178822c5373fb6e
danmalmx <danmalmx@gmail.com>
2019-05-23 09:24:08 +0200

    Adding InputCash method to VendingEngine class

--- Vending_Machine/Vending_Machine/VendingEngine.cs
+++ Vending_Machine/Vending_Machine/VendingEngine.cs
@@ -8,8 +8,25 @@ namespace Vending_Machine
 {
     class VendingEngine : Products
     {
-
         public Product ProductType { get; set; }
+        
+        public void InputCash()
+        {
+            int cashAmount = 0;
 
+            while (true)
+            {
+                Console.Write("Enter cash, end with 0: ");
+                int cashIn = int.Parse(Console.ReadLine());
+
+                if (cashIn == 0)
+                {
+
+                }
+                cashAmount += cashIn;
+            }
+        }
+
+        }
     }
 }
